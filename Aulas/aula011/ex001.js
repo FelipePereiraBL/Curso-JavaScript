ee907d7c694e657f9944f9ca458f24524b614628
FelipePereiraBL <68571815+FelipePereiraBL@users.noreply.github.com>
2022-06-08 21:45:39 -0300

    condiçoes - parte1

---
+++ Aulas/aula011/ex001.js
@@ -0,0 +1,9 @@
+var vel = 60,lim=12
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if(vel>lim){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Use sempre o cinto de segurança')
