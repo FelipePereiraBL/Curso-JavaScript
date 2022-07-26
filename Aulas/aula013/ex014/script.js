de75b354b373b2fd1433a77e3fb7c28d26fcd1ea
FelipePereiraBL <68571815+FelipePereiraBL@users.noreply.github.com>
2022-07-26 17:29:57 -0300

    ex- hora do dia

---
+++ Aulas/aula013/ex014/script.js
@@ -0,0 +1,24 @@
+function carregar(){
+    //Div onde vai aparecer a mensagem
+    var msg= document.getElementById('msg')
+    //Imagem que vai ser carregada
+    var img=document.getElementById('imagem')
+
+    //Hora atual do sistema
+    var hora=new Date().getHours()+":"+new Date().getMinutes();  
+
+    msg.innerHTML=`Agora são ${hora} horas`
+
+    if(hora >= 0 && hora < 12){
+        img.src='imagens/manha.png'
+        document.body.style.background='#ffe9bb'
+    }
+    else if(hora >= 12 && hora <= 18){
+        img.src='imagens/tarde.png'
+        document.body.style.background='#fa8000';
+    }
+    else{
+        img.src='imagens/noite.png'
+        document.body.style.background='#142b33'
+    }
+}
