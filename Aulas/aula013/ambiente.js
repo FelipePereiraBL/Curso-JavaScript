7f249216e7918ee5a863f5077ed40c5f0cdefaae
FelipePereiraBL <68571815+FelipePereiraBL@users.noreply.github.com>
2022-07-28 16:04:43 -0300

    while e do-while

---
+++ Aulas/aula013/ambiente.js
@@ -0,0 +1,21 @@
+/*
+console.log('Tudo Bem ?')
+console.log('Tudo Bem ?')
+console.log('Tudo Bem ?')
+console.log('Tudo Bem ?')
+console.log('Tudo Bem ?')
+ */
+
+var c = 1
+
+/*
+while(c<=500){
+    console.log(`${c}-while`)
+    c++
+}
+*/
+
+do{
+    console.log(`${c}-do while`)
+    c++
+}while(c<=10)
