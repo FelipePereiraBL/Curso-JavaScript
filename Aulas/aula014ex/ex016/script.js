f6eeee738733e02dc53f75e70ba1e7594985ad20
FelipePereiraBL <68571815+FelipePereiraBL@users.noreply.github.com>
2022-08-02 11:02:54 -0300

    exercicio pronto

--- Aulas/aula014ex/ex016/script.js
+++ Aulas/aula014ex/ex016/script.js
@@ -1,1 +1,26 @@
-alert("Olá")
+function calcular(){
+    var inic=Number(document.getElementById("idInic").value) 
+    var fim=Number(document.getElementById("idFim").value)
+    var passo=Number(document.getElementById("idPasso").value)
+
+    var res=document.querySelector("#result")
+
+    res.innerHTML=`<p></p>`
+
+    if(passo<=0){
+        window.alert("Passo invalido. Considerando PASSO 1")
+        passo=1
+    }
+
+    if(inic<=0 || fim<=0){
+        res.innerHTML+=`Impossivel contar`
+    }else{
+        for(inic;inic<=fim;inic+=passo){
+
+            res.innerHTML+=` ${inic} &#x1F449;`    
+        }
+    
+        res.innerHTML+=` &#x1F3C1`
+    }
+    
+}
