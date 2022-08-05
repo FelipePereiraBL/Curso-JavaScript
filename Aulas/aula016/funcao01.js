1cfb50092555dc869dcbb460b6713a2387b553f0
FelipePereiraBL <68571815+FelipePereiraBL@users.noreply.github.com>
2022-08-04 23:39:31 -0300

    funções part1

---
+++ Aulas/aula016/funcao01.js
@@ -0,0 +1,11 @@
+function parImp(valor){
+    let res
+
+    valor%2==0? res= 'Par!':res= 'Impar!'
+
+    return res
+}
+
+let numero=15
+
+console.log(parImp(numero))
---
+++ Aulas/aula016/funcao02.js
@@ -0,0 +1,8 @@
+function soma(n1=0, n2=0){
+    return n1+n2
+}
+
+let num1=34
+let num2=9
+
+console.log(soma(num1,num2))
